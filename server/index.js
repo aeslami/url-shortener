13400840bef3ac6813f68e4b0fc90ee6e326e8f7
Amir Harandi <a.eslami76@gmail.com>
2018-12-26 18:13:44 +0000

    Add basic functionality

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
-
+const Url = require('./models/Url');
 const port = process.env.PORT || 3000;
 const app = express();
+app.use(express.json());
 
-app.get('/:shortUrl', (req, res) => {
-  res.end(`Requested URL: ${req.params.shortUrl}`);
+app.post('/add', async (req, res) => {
+  const { fullURL } = req.body;
+  const result = await Url.create({ fullURL });
+  res.end(result.id);
+});
+
+app.get('/:shortURL', async (req, res) => {
+  const { shortURL } = req.params;
+  const { fullURL } = await Url.findById(shortURL);
+  res.status(301).redirect(fullURL);
 });
 
 app.listen(port, err => {
