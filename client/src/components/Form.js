20f321705fd4bbf1e2ea0671f53ed1df6038f838
Amir Harandi <a.eslami76@gmail.com>
2018-12-28 19:58:02 +0000

    MVP ready

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,10 +1,73 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fullURL: '',
+      shortURL: '',
+      error: ''
+    };
+  }
+
+  initialState = {
+    fullURL: '',
+    shortURL: '',
+    error: ''
+  };
+
+  onURLChange = e => {
+    this.setState({ fullURL: e.target.value });
+  };
+
+  onFormSubmit = async e => {
+    e.preventDefault();
+    try {
+      const fullURL = new URL(this.state.fullURL);
+      const response = await fetch('/add', {
+        method: 'POST',
+        body: JSON.stringify({ fullURL: fullURL.href }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      if (!response.ok) {
+        throw new Error(response.status); // 404
+      }
+      const jsonResponse = await response.json();
+      this.setState({ shortURL: jsonResponse.shortURL });
+    } catch (error) {
+      this.setState({ error: 'Error occurred' });
+    }
+  };
 
   render() {
-    return <div>Hi</div>;
+    return (
+      <div>
+        {this.state.shortURL.length == 0 && (
+          <form onSubmit={this.onFormSubmit}>
+            <input
+              type="url"
+              placeholder="Enter URL Here"
+              value={this.state.fullURL}
+              onChange={this.onURLChange}
+            />
+            <button>Submit</button>
+          </form>
+        )}
+        {this.state.shortURL.length > 0 && this.state.error.length == 0 && (
+          <div>
+            <h2>{this.state.shortURL}</h2>
+            <br />
+            <button onClick={() => this.setState(this.initialState)}>
+              Shorten another link
+            </button>
+          </div>
+        )}
+        {this.state.error.length > 0 && <p>{this.state.error}</p>}
+      </div>
+    );
   }
 }
 
